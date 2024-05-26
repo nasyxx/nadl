34bbbe195c911f5b77155496f9f7f0a989f350b3
Nasy <nasyxx+git@gmail.com>
2024-05-26 14:05:12 -0500

    Mark pad array as bool

--- src/nadl/data.py
+++ src/nadl/data.py
@@ -120,7 +120,9 @@ class IdxDataloader[T](eqx.Module):
 
     idxes = jnp.r_[idxes, jnp.full(self.pad, -1, idxes.dtype)]
     idxes = idxes[: length + self.pad].reshape(-1, self.batch_size)
-    return DState(self.transform(idxes), jnp.where(idxes == -1, 1, 0), idxes.shape)
+    return DState(
+      self.transform(idxes), jnp.where(idxes == -1, 0, 1).astype(bool), idxes.shape
+    )
 
 
 def es_loop[T](
